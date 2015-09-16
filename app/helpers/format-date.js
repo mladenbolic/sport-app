4ab5fb88abdd2f36974a784ca26d141c0932f03e
Mladen Bolic <mbolic@deployinc.com>
2015-09-17 00:06:11 +0200

    SA: use the US date format.

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -4,5 +4,5 @@ import Ember from 'ember';
  * Helper for formatting dates.
  */
 export default Ember.Handlebars.makeBoundHelper(function (value) {
-    return window.moment(value).format('DD.MM.YYYY, HH:mm');
+    return window.moment(value).format('MM/DD/YYYY, HH:mm');
 });
